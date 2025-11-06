b8069a6bc5d6b6443ed05fbdeb9eb646c289cdad
Teixeira49 <javierarturoteixeiragoncalves@gmail.com>
2025-11-06 11:21:46 -0400

    Refactor dollar_controller to use instance methods of DollarService

--- api/controller/dollar_controller.py
+++ api/controller/dollar_controller.py
@@ -5,13 +5,15 @@ from api.services.dollar_services import DollarService
 
 router = APIRouter(prefix="/api")
 
+dollar_service = DollarService() # Crea una instancia de DollarService
+
 class FilterParams:
     actualRate: bool = Field(True)
     
 @router.put("/update-latest-exchange-rate")
 async def get_latest_exchange_rate():
     try:
-        exchange_rate = await DollarService.getCurrenciesByBCV()
+        exchange_rate = await dollar_service.getCurrenciesByBCV() # Llama al método desde la instancia
         return {"exchange_rate": exchange_rate}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
@@ -19,7 +21,7 @@ async def get_latest_exchange_rate():
 @router.get("/actual-exchange-rate")
 async def get_saved_currencies(today_data: Optional[bool] = Query(None, alias="today-data")):
     try:
-        currencies = await DollarService.getSavedCurrencies(today_data)
+        currencies = await dollar_service.getSavedCurrencies(today_data) # Llama al método desde la instancia
         return {"exchange_rate": currencies}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
