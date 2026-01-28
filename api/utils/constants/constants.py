aa3c4ee3ef66262e7d19c31dbc72849cfba1365c
Teixeira49 <javierarturoteixeiragoncalves@gmail.com>
2026-01-27 23:06:26 -0400

    implement new constants divided in business politics

---
+++ api/utils/constants/constants.py
@@ -0,0 +1,29 @@
+class Constants:
+
+    EMPTY_STRING = ""
+
+    EMPTY_SPACE = " "
+
+    PAGE_LIMIT = 10
+
+    F_HTML = 'html.parser'
+
+    VERIFY = False
+
+    STATUS_OK = 200
+
+    STATUS_OK_MSG = 'Success'
+
+    STATUS_OK_DEATILS = 'Elemento retornado con exito'
+
+    STATUS_NOT_FOUND = 404
+
+    STATUS_NOT_FOUND_MSG = 'Not Found'
+
+    STATUS_INTERNAL_SERVER_ERROR = 500
+
+    STATUS_INTERNAL_SERVER_ERROR_MSG = 'Internal Server Error'
+
+    BCV_NAME = 'Banco Central de Venezuela'
+    
+    BCV_LOGO_URL = 'https://scontent-bog2-2.cdninstagram.com/v/t51.2885-19/23347396_311015296045023_2037755449859637248_n.jpg?efg=eyJ2ZW5jb2RlX3RhZyI6InByb2ZpbGVfcGljLmRqYW5nby41MTIuYzIifQ&_nc_ht=scontent-bog2-2.cdninstagram.com&_nc_cat=1&_nc_oc=Q6cZ2QFRep9wcXdFz3AKBjoQtyuA23gWawobtZda-NkHp4DOEM4hKuWkykSGjiHwRKyUGqw&_nc_ohc=t6p6iILf0OMQ7kNvwEViQPO&_nc_gid=WQjmzh4-GqDqxxHYZIT2Ig&edm=APoiHPcBAAAA&ccb=7-5&oh=00_AfrjnXUmXml8OSzFR6h53_MkK8aKDFzwcFOtT6ebqMX5Yg&oe=697F316D&_nc_sid=22de04'
---
+++ api/utils/constants/scrapping_tags.py
@@ -0,0 +1,18 @@
+
+class ScrappingTags:
+
+    ID_DOLAR = "dolar"
+
+    KEY_DATE = "content"
+
+    KEY_NAME = "id"
+
+    CLASS_DATE = "date-display-single"
+
+    CLASS_CURRENCY = "col-sm-12 col-xs-12"
+
+    CLASS_IMAGE = "icono_bss_blanco1"
+
+    CLASS_CODE = "span"
+
+    CLASS_NAME = "strong"
