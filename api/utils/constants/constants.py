85b5a2ce1a7746e25c52ca48addda670c0096b8d
Teixeira49 <javierarturoteixeiragoncalves@gmail.com>
2026-01-28 11:50:29 -0400

    add new constants for app name and version

--- api/utils/constants/constants.py
+++ api/utils/constants/constants.py
@@ -1,5 +1,9 @@
 class Constants:
 
+    APP_NAME = "DolarTracker"
+
+    VERSION = "1.1.0"
+
     EMPTY_STRING = ""
 
     EMPTY_SPACE = " "
@@ -27,3 +31,11 @@ class Constants:
     BCV_NAME = 'Banco Central de Venezuela'
     
     BCV_LOGO_URL = 'https://scontent-bog2-2.cdninstagram.com/v/t51.2885-19/23347396_311015296045023_2037755449859637248_n.jpg?efg=eyJ2ZW5jb2RlX3RhZyI6InByb2ZpbGVfcGljLmRqYW5nby41MTIuYzIifQ&_nc_ht=scontent-bog2-2.cdninstagram.com&_nc_cat=1&_nc_oc=Q6cZ2QFRep9wcXdFz3AKBjoQtyuA23gWawobtZda-NkHp4DOEM4hKuWkykSGjiHwRKyUGqw&_nc_ohc=t6p6iILf0OMQ7kNvwEViQPO&_nc_gid=WQjmzh4-GqDqxxHYZIT2Ig&edm=APoiHPcBAAAA&ccb=7-5&oh=00_AfrjnXUmXml8OSzFR6h53_MkK8aKDFzwcFOtT6ebqMX5Yg&oe=697F316D&_nc_sid=22de04'
+
+    YADIO_NAME = 'Yadio.io'
+
+    YADIO_LOGO_URL = 'https://scontent-bog2-2.cdninstagram.com/v/t51.2885-19/49818634_637292953340038_257429348900929536_n.jpg?efg=eyJ2ZW5jb2RlX3RhZyI6InByb2ZpbGVfcGljLmRqYW5nby45MzMuYzIifQ&_nc_ht=scontent-bog2-2.cdninstagram.com&_nc_cat=110&_nc_oc=Q6cZ2QGKzDRG-F5L-2vqnvB44ygeUwtby10vUZi6uOmNxsEYse0hpiiR-5n1JqkXC3QUp58&_nc_ohc=AZFy9WiGJloQ7kNvwEjF_St&_nc_gid=1uU1UW3S3d7scrcQRw6fzA&edm=AP4sbd4BAAAA&ccb=7-5&oh=00_AfrLCrGd8EJP_nju72-s3b3mwFnuPoL8zwcwwm-CYSKjIQ&oe=697F56CD&_nc_sid=7a9f4b'
+
+    BINANCE_NAME = 'Binance'
+
+    BINANCE_LOGO_URL = 'https://public.bnbstatic.com/20190405/eb2349c3-b2f8-4a93-a286-8f86a62ea9d8.png'
