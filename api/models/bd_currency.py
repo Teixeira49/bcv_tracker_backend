a63f988f30e7507748ac933163789cd2730f7f6a
Teixeira49 <javierarturoteixeiragoncalves@gmail.com>
2026-01-28 11:12:52 -0400

    refactor currency bd structure

--- api/models/bd_currency.py
+++ api/models/bd_currency.py
@@ -9,11 +9,20 @@ class Currency(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     code = Column(String, nullable=False, index=True)
     name = Column(String)
-    linkImage = Column(String)
-    exchangeRate = Column(Float)
+    platform = Column(String)
+    value = Column(Float)
     createDate = Column(DateTime, default=func.now(), nullable=False)
     updateDate = Column(DateTime, default=func.now(), onupdate=func.now(), nullable=False)
-    todayData = Column(Boolean, default=False, nullable=False)
 
     def __repr__(self):
-        return f"<Currency(code={self.code}, exchangeRate={self.exchangeRate})>"
+        return f"<Currency(code={self.code}, value={self.value})>"
+    
+    def to_dict(self):
+        return {
+            "code": self.code,
+            "name": self.name,
+            "platform": self.platform,
+            "value": self.value,
+            "createDate": self.createDate.isoformat() if self.createDate else None,
+            "updateDate": self.updateDate.isoformat() if self.updateDate else None,
+        }
--- api/services/bd_service.py
+++ api/services/bd_service.py
@@ -1,14 +1,14 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, inspect
 from sqlalchemy.orm import sessionmaker
-import os
 from typing import List
 
+from api.core.config.config import Config as c
 from ..models.bd_currency import Base, Currency
-from api.utils.helper import Helper
+from api.utils.helpers.helper import Helper
 
 # Lee la URL de la base de datos desde las variables de entorno.
 # El archivo .env que mostraste contiene esta variable. Vercel la inyectará automáticamente.
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = c.DATABASE_URL
 
 if not DATABASE_URL:
     raise ValueError("No DATABASE_URL environment variable set")
@@ -20,6 +20,18 @@ def init_db():
     """Crear tablas si no existen."""
     Base.metadata.create_all(bind=engine)
 
+def reset_db():
+    """Comprueba si la tabla existe y la elimina para reiniciarla."""
+    inspector = inspect(engine)
+    # Verificamos si la tabla 'currencies' existe
+    if inspector.has_table(Currency.__tablename__):
+        print(f"Reiniciando tabla: {Currency.__tablename__}")
+        # Eliminamos solo la tabla específica
+        Currency.__table__.drop(engine)
+    
+    # Volvemos a crear las tablas
+    init_db()
+
 def save_currencies_to_db(currencies: List[Currency]):
 
     init_db()
@@ -30,15 +42,14 @@ def save_currencies_to_db(currencies: List[Currency]):
             # Intenta obtener el registro existente con el mismo código y valor de todayData
             existing_row = session.query(Currency).filter(
                 Currency.code == cur.code,
-                Currency.todayData == cur.todayData
+                Currency.platform == cur.platform,
             ).first()
 
             # actualizar o crear el registro "todayData == True"
             if existing_row:
                 # Si existe un registro con el mismo código y todayData, actualízalo
                 existing_row.name = cur.name
-                existing_row.linkImage = cur.linkImage
-                existing_row.exchangeRate = cur.exchangeRate
+                existing_row.value = cur.value
                 existing_row.updateDate = now
             # actualizar o crear el registro "todayData == False"
             else:
@@ -46,11 +57,12 @@ def save_currencies_to_db(currencies: List[Currency]):
                 new_currency = Currency(
                     code=cur.code,
                     name=cur.name,
-                    linkImage=cur.linkImage,
-                    exchangeRate=cur.exchangeRate,
+                    platform=cur.platform,
+                    platformLinkImage=cur.platformLinkImage,
+                    symbolLinkImage=cur.symbolLinkImage,
+                    value=cur.value,
                     createDate=now,
                     updateDate=now,
-                    todayData=cur.todayData
                 )
                 session.add(new_currency)
 
